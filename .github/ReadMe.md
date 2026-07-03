9f0198475f58550423fa686bce2c56e2ea94fd56
moke <18336485360@163.com>
2026-07-03 11:24:04 +0800

    add RapidXml

--- .github/ReadMe.md
+++ .github/ReadMe.md
@@ -4,4 +4,8 @@ This fork is for personal learning.
 
 ## Changes
 
-1. Rename `3rdlib` to `vendor`.
+1. Reorganize directory structure
+
+2. Rename `3rdlib` to `vendor`.
+
+3. Replace `Lyra` to `argp`
---
+++ game/tools/schema_parser/main.cpp
@@ -0,0 +1,13 @@
+#include <argp/argp.h>
+#include <rapidxml.hpp>
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    rapidxml::xml_document<> doc;
+    std::string data = "<welcome>Hello RapidXML</welcome>";
+    doc.parse<0>(data.data());
+    rapidxml::xml_node<>* node = doc.first_node("welcome");
+    std::cout << node->value() << std::endl;
+    return 0;
+}
